4dfc47b071b160abde8629fe1a00e4a2f3d5a0dc
Roger D. Winans <solvaholic@users.noreply.github.com>
2021-10-10 20:31:29 -0400

    Get terraform apply to create most resources
    - Add identity to azurerm_function_app
    - Place key vault in resource group's location
    - Use generic names for resources
    - Remove key vault access policy for user
    
    #2

--- main.tf
+++ main.tf
@@ -21,9 +21,9 @@ resource "azurerm_resource_group" "push-log" {
   location = "Central US"
 }
 
-# Storage account: storageaccountpushl9725
-resource "azurerm_storage_account" "storageaccountpushl9725" {
-  name                     = "storageaccountpushl9725"
+# Storage account: pushlogstorage
+resource "azurerm_storage_account" "pushlogstorage" {
+  name                     = "pushlogstorage"
   account_kind             = "Storage"
   min_tls_version          = "TLS1_2"
   resource_group_name      = azurerm_resource_group.push-log.name
@@ -32,9 +32,9 @@ resource "azurerm_storage_account" "storageaccountpushl9725" {
   account_replication_type = "LRS"
 }
 
-# Service plan: ASP-pushlog-94c0
-resource "azurerm_app_service_plan" "ASP-pushlog-94c0" {
-  name                = "ASP-pushlog-94c0"
+# Service plan: push-log-svc-plan
+resource "azurerm_app_service_plan" "push-log-svc-plan" {
+  name                = "push-log-svc-plan"
   location            = azurerm_resource_group.push-log.location
   resource_group_name = azurerm_resource_group.push-log.name
   kind                = "FunctionApp"
@@ -48,92 +48,36 @@ resource "azurerm_app_service_plan" "ASP-pushlog-94c0" {
 # Function app: solvhaolic-push-log
 resource "azurerm_function_app" "solvhaolic-push-log" {
   name                       = "solvhaolic-push-log"
-  app_service_plan_id        = azurerm_app_service_plan.ASP-pushlog-94c0.id
+  app_service_plan_id        = azurerm_app_service_plan.push-log-svc-plan.id
   enable_builtin_logging     = false
+  identity {
+    type = "SystemAssigned"
+  }
   location                   = azurerm_resource_group.push-log.location
   resource_group_name        = azurerm_resource_group.push-log.name
-  storage_account_access_key = azurerm_storage_account.storageaccountpushl9725.primary_access_key
-  storage_account_name       = azurerm_storage_account.storageaccountpushl9725.name
+  storage_account_access_key = azurerm_storage_account.pushlogstorage.primary_access_key
+  storage_account_name       = azurerm_storage_account.pushlogstorage.name
   version                    = "~3"
 }
 
 # Key vault: push-log-keyvault
 resource "azurerm_key_vault" "push-log-keyvault" {
   name                = "push-log-keyvault"
-  enable_rbac_authorization = false
-  enabled_for_deployment = false
-  enabled_for_disk_encryption = false
-  enabled_for_template_deployment = false
-  location            = "eastus"
+  location            = azurerm_resource_group.push-log.location
   purge_protection_enabled = false
   resource_group_name = azurerm_resource_group.push-log.name
   sku_name            = "standard"
-  tags                = {}
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  access_policy = [
-    { # Permit the application to read secrets
-      application_id = ""
-      tenant_id = data.azurerm_client_config.current.tenant_id
-      object_id = azurerm_function_app.solvhaolic-push-log.identity.0.principal_id
-      certificate_permissions = []
-      key_permissions = []
-      secret_permissions = [
-        "Get",
-        "List",
-      ]
-      storage_permissions = []
-    },
-    { # Permit the authenticated user to admin
-      application_id = ""
-      tenant_id = data.azurerm_client_config.current.tenant_id
-      object_id = data.azurerm_client_config.current.object_id
-      "certificate_permissions": [
-        "Get",
-        "List",
-        "Delete",
-        "Create",
-        "Import",
-        "Update",
-        "ManageContacts",
-        "GetIssuers",
-        "ListIssuers",
-        "SetIssuers",
-        "DeleteIssuers",
-        "ManageIssuers",
-        "Recover"
-      ],
-      "key_permissions": [
-        "Get",
-        "Create",
-        "Delete",
-        "List",
-        "Update",
-        "Import",
-        "Backup",
-        "Restore",
-        "Recover"
-      ],
-      "secret_permissions": [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Backup",
-        "Restore",
-        "Recover"
-      ],
-      "storage_permissions": [
-        "Get",
-        "List",
-        "Delete",
-        "Set",
-        "Update",
-        "RegenerateKey",
-        "SetSAS",
-        "ListSAS",
-        "GetSAS",
-        "DeleteSAS"
-      ],
-    }
-  ]
+  access_policy { # Permit the application to read secrets
+    application_id = ""
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_function_app.solvhaolic-push-log.identity.0.principal_id
+    certificate_permissions = []
+    key_permissions = []
+    secret_permissions = [
+      "Get",
+      "List",
+    ]
+    storage_permissions = []
+  }
 }
